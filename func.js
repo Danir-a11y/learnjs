f2ad2dc265658ab3af2e5d0ebff60275842ce664
Danir-a11y <danirchik1@gmail.com>
2025-04-11 14:55:35 +0500

    Add files via upload

---
+++ func.js
@@ -0,0 +1,14 @@
+alert("Вот тут мне и понадобился отдельный javascript файл")
+
+
+function vsemprivet() {
+    alert("Всем привет!")
+}
+
+let username = prompt(" Как тебя зовут? ")
+
+function name() {
+    alert(`Привет, ${username}`)
+}
+
+name()
